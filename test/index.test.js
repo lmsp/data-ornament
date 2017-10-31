932f52ee556a5518030545072fe926e034ba1551
lmsp <lmsp@opoloxoloqo.org>
2017-10-31 20:55:43 +0100

    Actualizar CHANGELOG.md

--- test/index.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-/* global test */
-/* @flow */
-import Index from '../src/index'
-
-test('index test', () => {
-  const index = new Index()
-  expect(index.name).toEqual('MyIndex')
-})
